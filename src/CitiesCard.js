68eee29e6f2a290dd03ae38bde14c692c4e5c7fb
Andrew <andrewchoi1@hotmail.com>
2018-10-02 14:51:09 -0400

    fixed searchbar

--- src/CitiesCard.js
+++ src/CitiesCard.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CityCard from './CityCard';
 
 const CitiesCard = (props) => {
+  console.log(props)
   return <div>
     {
       props.citiesData.map(cityData => <CityCard key={cityData.id} cityData={cityData} />)
