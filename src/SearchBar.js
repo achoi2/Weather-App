dd9b81a474d6a90e7806338a1e59e4e6ccd67ca3
Andrew <andrewchoi1@hotmail.com>
2018-10-02 13:38:21 -0400

    connected searchbar with fetch request

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <div>
-        SearchBar
-  </div>
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: ''
+        };
+    }
+
+    render() {
+        let submitForm = (event) => {
+            event.preventDefault();
+        }
+
+        return (
+            <div>
+                <form onSubmit={submitForm}>
+                    <input placeholder="enter a zip code" value={this.state.term} onChange={this.props.changeInput}/>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
