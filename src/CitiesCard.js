f38be9ac188cb4a270ff585fd0a931b9ddf7ca7e
Andrew <andrewchoi1@hotmail.com>
2018-10-02 08:28:56 -0400

    created components

---
+++ src/CitiesCard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import CityCard from './CityCard';
+
+const CitiesCard = () => {
+  return <div>
+    CitiesCard
+    <CityCard />
+  </div>
+}
+
+export default CitiesCard;
---
+++ src/Weatherviewer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import CitiesCard from './CitiesCard';
+
+class Weatherviewer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cities: [
+                { id: '1', city: 'atlanta', temp: 90 },
+                { id: '2', city: 'miami', temp: 95 },
+                { id: '3', city: 'new york', temp: 85 }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>
+                    {this.state.cities.map(city => (
+                        <div>{city}</div>
+                    ))}
+                </h3>
+                <CitiesCard />
+            </div>
+        );
+    }
+}
+
+export default Weatherviewer;
