3708752d60de7fee25d4655639434ede349147be
Andrew <andrewchoi1@hotmail.com>
2018-10-03 09:57:12 -0400

    added header

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <div>
+        <h1>Weather App</h1>
+        <h2>Check the weather for your city!</h2>
+    </div>
+}
+
+export default Header;
